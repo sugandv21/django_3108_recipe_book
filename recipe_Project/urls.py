60ed2240dcf292de53329c34f9435193e5a9144e
sugandv21 <sdvsuganya@gmail.com>
2025-08-31 20:59:07 +0530

    Update urls.py

--- recipe_Project/urls.py
+++ recipe_Project/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),  
     path("", include("recipe.urls")),
 ]
 
