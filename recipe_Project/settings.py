fe0534e287063e1090a2098edb5db9ef8f64dc38
sugandv21 <sdvsuganya@gmail.com>
2025-08-31 20:58:41 +0530

    Update settings.py

--- recipe_Project/settings.py
+++ recipe_Project/settings.py
@@ -126,8 +126,8 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
 
-LOGIN_URL = "login"
-LOGOUT_REDIRECT_URL = "home"
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
