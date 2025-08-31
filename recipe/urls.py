6a7c2b67f7ed1fbc9828333fff2e50c5eadb9674
sugandv21 <sdvsuganya@gmail.com>
2025-08-31 21:00:45 +0530

    Update urls.py

--- recipe/urls.py
+++ recipe/urls.py
@@ -8,13 +8,22 @@ from .views import (
 
 urlpatterns = [
     path("", home, name="home"),
-    path("recipe/", RecipeListView.as_view(), name="recipe_list"),
+
+    path("recipes/", RecipeListView.as_view(), name="recipe_list"),
     path("recipe/<int:pk>/", RecipeDetailView.as_view(), name="recipe_detail"),
     path("recipe/new/", recipe_create, name="recipe_create"),
     path("recipe/<int:pk>/edit/", recipe_edit, name="recipe_edit"),
     path("recipe/<int:pk>/delete/", recipe_delete, name="recipe_delete"),
 
-    path("accounts/login/", auth_views.LoginView.as_view(template_name="recipe/layout/login.html"), name="login"),
-    path("accounts/logout/", auth_views.LogoutView.as_view(template_name="recipe/layout/logged_out.html"), name="logout"),
+    path(
+        "accounts/login/",
+        auth_views.LoginView.as_view(template_name="recipe/layout/login.html"),
+        name="login"
+    ),
+    path(
+        "accounts/logout/",
+        auth_views.LogoutView.as_view(template_name="recipe/layout/logged_out.html"),
+        name="logout"
+    ),
     path("register/", register, name="register"),
 ]
